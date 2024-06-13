85eb7bfe5c5c4d9a92bb65bf9ee402adc0c76cd9
Nolan Cretney <nokynokes@gmail.com>
2024-06-12 20:46:40 -0600

    shapes

--- src/Shapes.elm
+++ src/Shapes.elm
@@ -1,23 +1,59 @@
 module Shapes exposing (..)
 
 type Shape2D = Circle | Square | Triangle 
+type Shape3D 
+    = Sphere 
+    | Cube
+    | Pyramid
+    | Cylinder
+    | Cone
+    | Prism
 
-type Shape3D = Combine Shape2D Shape2D
+combine : Shape2D -> Shape2D -> Shape3D
+combine shape1 shape2 = 
+    case (shape1, shape2) of
+        (Circle, Circle) -> Sphere
+        (Square, Square) -> Cube
+        (Triangle, Triangle) -> Pyramid 
+        (Circle, Square) -> Cylinder
+        (Square, Circle) -> Cylinder
+        (Circle, Triangle) -> Cone
+        (Triangle, Circle) -> Cone
+        (Square, Triangle) -> Prism
+        (Triangle, Square) -> Prism
 
-sphere : Shape3D
-sphere = Combine Circle Circle
+subtract : Shape2D -> Shape3D -> Shape2D
+subtract dissect shape = 
+    case (dissect, shape) of
+        (Circle, Sphere) -> Circle
+        (Square, Cube) -> Square
+        (Triangle, Pyramid) -> Triangle
+        (Circle, Cylinder) -> Square
+        (Square, Cylinder) -> Circle
+        (Circle, Cone) -> Triangle
+        (Triangle, Cone) -> Circle
+        (Square, Prism) -> Triangle
+        (Triangle, Prism) -> Square
+        _ -> dissect
 
-cube : Shape3D
-cube = Combine Square Square
+-- type Shape3D = Combine Shape2D Shape2D
 
-pyramid : Shape3D
-pyramid = Combine Triangle Triangle
+-- sphere : Shape3D
+-- sphere = Combine Circle Circle
 
-cylinder : Shape3D
-cylinder = Combine Circle Square
+-- cube : Shape3D
+-- cube = Combine Square Square
 
-cone : Shape3D
-cone = Combine Circle Triangle
+-- pyramid : Shape3D
+-- pyramid = Combine Triangle Triangle
 
-prism : Shape3D
-prism = Combine Square Triangle
+-- cylinder : Shape3D
+-- cylinder = Combine Circle Square
+
+-- cone : Shape3D
+-- cone = Combine Circle Triangle
+
+-- prism : Shape3D
+-- prism = Combine Square Triangle
+
+-- func = Combine Circle
