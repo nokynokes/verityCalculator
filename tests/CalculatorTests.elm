40e6c3ecee87fde7982f0a82ecb3e9bb35432e18
Nolan Cretney <nokynokes@gmail.com>
2024-06-13 10:49:53 -0600

    more tests

--- tests/CalculatorTests.elm
+++ tests/CalculatorTests.elm
@@ -28,4 +28,38 @@ calculatorSuite =
                         Nothing -> Expect.pass
                         _ -> Expect.fail "should not be able to create any new shapes"
             ]
+        , describe "Sqaure"
+            [ test "should swap with Circle" <|
+                \_ ->
+                    case dissectShapes (Square, Cube) (Circle, Sphere) of
+                        Just (Cylinder, Cylinder) -> Expect.pass
+                        _ -> Expect.fail "shoud both be Cylinder"
+            , test "should swap with Triangle" <|
+                \_ -> 
+                    case dissectShapes (Square, Cylinder) (Triangle, Cone) of
+                        Just (Cone, Cylinder) -> Expect.pass
+                        _ -> Expect.fail "should be Cone and Cylinder"
+            , test "should not be able to swap" <|
+                \_ -> 
+                    case dissectShapes (Square, Cone) (Circle, Prism) of
+                        Nothing -> Expect.pass
+                        _ -> Expect.fail "should not be able to create any new shapes"
+            ]
+        , describe "Triangle"
+            [ test "should swap with Circle" <|
+                \_ ->
+                    case dissectShapes (Triangle, Pyramid) (Circle, Sphere) of
+                        Just (Cone, Cone) -> Expect.pass
+                        _ -> Expect.fail "shoud both be Cone"
+            , test "should swap with Square" <|
+                \_ -> 
+                    case dissectShapes (Triangle, Prism) (Square, Cylinder) of
+                        Just (Cube, Cone) -> Expect.pass
+                        _ -> Expect.fail "should be Cube and Cone"
+            , test "should not be able to swap" <|
+                \_ -> 
+                    case dissectShapes (Triangle, Cylinder) (Circle, Prism) of
+                        Nothing -> Expect.pass
+                        _ -> Expect.fail "should not be able to create any new shapes"
+            ]
         ]
