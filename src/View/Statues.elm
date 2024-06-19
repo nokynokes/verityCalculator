70dfff39f78396a2479f1bf49e604d7fab36865e
Nolan Cretney <nokynokes@gmail.com>
2024-06-19 10:11:22 -0600

    radio group buttons working

--- src/View/Statues.elm
+++ src/View/Statues.elm
@@ -1,6 +1,6 @@
 module View.Statues exposing (renderStatue)
 
-import Css
+import Css exposing (position, rad)
 import Css.Global
 import Html.Styled as Html exposing (Html, div, h2, text)
 import Html.Styled.Attributes as Html exposing (css)
@@ -10,18 +10,23 @@ import Tailwind.Theme as Theme
 import Tailwind.Utilities as Tw
 
 
-radioButton : String -> Html msg
-radioButton name =
+radioButton : Position -> String -> String -> Html msg
+radioButton position className name =
+    let
+        id =
+            toString position ++ name
+    in
     Html.div
         [ Html.css
             [ Tw.flex
             , Tw.flex_col
             ]
-        , Html.class "radio-button"
+        , Html.class className
         ]
         [ Html.input
             [ Html.type_ "radio"
-            , Html.id "myCustomRadio"
+            , Html.name className
+            , Html.id id
             , Html.css
                 [ Tw.hidden
                 , Css.checked
@@ -35,35 +40,39 @@ radioButton name =
             []
         , Html.label
             [ Html.class "radio-button__label"
-            , Html.for "myCustomRadioid"
+            , Html.for id
             , Html.css
-                [ Css.displayFlex
-                , Css.alignItems Css.center
-                , Css.justifyContent Css.center
-                , Css.padding <| Css.rem 0.8
-                , Css.cursor Css.pointer
+                [ Tw.flex
+                , Tw.items_center
+                , Tw.justify_center
+                , Tw.p_4
+                , Tw.cursor_pointer
                 ]
             ]
             [ text name ]
         ]
 
 
-radioButtonGroup : Html msg
-radioButtonGroup =
+radioButtonGroup : Position -> Html msg
+radioButtonGroup position =
+    let
+        radioButtonClass =
+            toString position ++ "radio-button"
+    in
     Html.div
         [ Html.css
             [ Css.displayFlex
             , Css.border3 (Css.px 1) Css.solid (Css.hex "efefef")
             , Css.borderRadius <| Css.px 4
             , Css.Global.descendants
-                [ Css.Global.selector ".radio-button:not(:last-of-type)"
+                [ Css.Global.selector ("." ++ radioButtonClass ++ ":not(:last-of-type)")
                     [ Css.borderRight3 (Css.px 1) Css.solid (Css.hex "efefef") ]
                 ]
             ]
         ]
     <|
         List.map
-            radioButton
+            (radioButton position radioButtonClass)
             [ "Circle", "Square", "Triangle" ]
 
 
@@ -71,5 +80,5 @@ renderStatue : Position -> Html msg
 renderStatue position =
     div [ css [ Tw.text_color Theme.white ] ]
         [ h2 [] [ toString position |> text ]
-        , radioButtonGroup
+        , radioButtonGroup position
         ]
