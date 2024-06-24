ae725f795c9a6e2e6f7e995bece24673c6941ad4
Nolan Cretney <nokynokes@gmail.com>
2024-06-24 11:52:43 -0600

    render steps

--- src/View/Steps.elm
+++ src/View/Steps.elm
@@ -1,10 +1,12 @@
 module View.Steps exposing (..)
 
+import Calculator exposing (dissectShapes)
 import Css exposing (Style)
-import Html.Styled as Html exposing (Html, div, h2, text)
+import Html.Styled as Html exposing (Html, div, h1, h2, h3, h4, text)
 import Html.Styled.Attributes as Html exposing (checked, css, step)
 import Msg exposing (Msg)
 import Shapes exposing (toString2D, toString3D)
+import Statues exposing (isComplete)
 import Statues.Internal exposing (Position(..), toString)
 import Steps.Internal exposing (StatueDissect, Step)
 import Tailwind.Breakpoints as Bp
@@ -17,26 +19,57 @@ stepOneBackGround =
     [ Tw.flex, Tw.flex_row, Tw.bg_gradient_to_b, Tw.from_color Theme.emerald_400, Tw.to_color Theme.emerald_800 ]
 
 
-stepForStatue : StatueDissect -> Html Msg
-stepForStatue dissect =
+stepTwoBackGround : List Style
+stepTwoBackGround =
+    [ Tw.flex, Tw.flex_row, Tw.bg_gradient_to_b, Tw.from_color Theme.blue_400, Tw.to_color Theme.blue_800 ]
+
+
+stepForStatue : Int -> StatueDissect -> Html Msg
+stepForStatue stepNumber dissect =
+    let
+        statueName =
+            toString dissect.statueAfterDissect.position
+
+        dissectShape =
+            toString2D dissect.shapeToDissect
+
+        finishedShape =
+            toString3D dissect.statueAfterDissect.outsideShape
+    in
     Html.div
         [ css [ Tw.basis_1over3, Tw.text_color Theme.white, Tw.text_4xl ] ]
-        [ (toString >> text) dissect.statueAfterDissect.position
-        , (toString2D >> text) dissect.shapeToDissect
-        , (toString3D >> text) dissect.statueAfterDissect.outsideShape
+        [ h1 [] [ (String.fromInt >> text) stepNumber ]
+        , h3 [] [ text statueName ]
+        , div
+            []
+            [ text <| "-" ++ dissectShape ]
+        , h4 [] [ text "After dissect:" ]
+        , div
+            []
+            [ text finishedShape ]
+        , if isComplete dissect.statueAfterDissect then
+            div
+                []
+                [ text <| statueName ++ " statue is now complete!" ]
+
+          else
+            div [] []
         ]
 
 
-renderStep : Step -> List (Html Msg)
-renderStep ( statue1, statue2 ) =
+renderStep : Step -> Int -> List (Html Msg)
+renderStep ( statue1, statue2 ) stepNumber =
     let
+        render =
+            stepForStatue stepNumber
+
         leftStatue =
             case ( statue1.statueAfterDissect.position, statue2.statueAfterDissect.position ) of
                 ( Left, _ ) ->
-                    stepForStatue statue1
+                    render statue1
 
                 ( _, Left ) ->
-                    stepForStatue statue2
+                    render statue2
 
                 _ ->
                     Html.div [ css [ Tw.basis_1over3 ] ] []
@@ -44,10 +77,10 @@ renderStep ( statue1, statue2 ) =
         middleStatue =
             case ( statue1.statueAfterDissect.position, statue2.statueAfterDissect.position ) of
                 ( Middle, _ ) ->
-                    stepForStatue statue1
+                    render statue1
 
                 ( _, Middle ) ->
-                    stepForStatue statue2
+                    render statue2
 
                 _ ->
                     Html.div [ css [ Tw.basis_1over3 ] ] []
@@ -55,10 +88,10 @@ renderStep ( statue1, statue2 ) =
         rightStatue =
             case ( statue1.statueAfterDissect.position, statue2.statueAfterDissect.position ) of
                 ( Right, _ ) ->
-                    stepForStatue statue1
+                    render statue1
 
                 ( _, Right ) ->
-                    stepForStatue statue2
+                    render statue2
 
                 _ ->
                     Html.div [ css [ Tw.basis_1over3 ] ] []
@@ -76,11 +109,14 @@ renderSteps_ stepNumber steps =
                         1 ->
                             stepOneBackGround
 
+                        2 ->
+                            stepTwoBackGround
+
                         _ ->
                             stepOneBackGround
 
                 container =
-                    renderStep step
+                    renderStep step stepNumber
                         |> div [ css backgroundGradient ]
             in
             container :: renderSteps_ (stepNumber + 1) tail
