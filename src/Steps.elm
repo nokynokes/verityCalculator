df502a52dee39a0d95e5efefc138ff14f51e74f9
Nolan Cretney <nokynokes@gmail.com>
2024-08-01 09:50:28 -0600

    inital commit

--- src/Steps.elm
+++ src/Steps.elm
@@ -8,33 +8,29 @@ generateSteps : List Statue -> List Step
 generateSteps statues =
     case statues of
         [ s1, s2, s3 ] ->
-            case generateStep s1 s2 of
-                Just ( step1, step2 ) ->
+            generateStep s1 s2
+                |> Maybe.andThen (\(step1, step2) -> 
                     if isComplete step1.statueAfterDissect.insideShape step1.statueAfterDissect.outsideShape then
-                        ( step1, step2 ) :: generateSteps [ { s2 | outsideShape = step2.statueAfterDissect.outsideShape }, s3 ]
+                         Just <| ( step1, step2 ) :: generateSteps [ { s2 | outsideShape = step2.statueAfterDissect.outsideShape }, s3 ]
 
                     else
-                        ( step1, step2 ) :: generateSteps [ { s1 | outsideShape = step1.statueAfterDissect.outsideShape }, { s2 | outsideShape = step2.statueAfterDissect.outsideShape }, s3 ]
-
-                Nothing ->
-                    case generateStep s1 s3 of
-                        Just ( step1, step3 ) ->
+                        Just <| ( step1, step2 ) :: generateSteps [ { s1 | outsideShape = step1.statueAfterDissect.outsideShape }, { s2 | outsideShape = step2.statueAfterDissect.outsideShape }, s3 ]
+                )
+                |> Maybe.withDefault 
+                    ( generateStep s1 s3
+                        |> Maybe.andThen (\(step1, step3) -> 
                             if isComplete step1.statueAfterDissect.insideShape step1.statueAfterDissect.outsideShape then
-                                ( step1, step3 ) :: generateSteps [ s2, { s3 | outsideShape = step3.statueAfterDissect.outsideShape } ]
+                                 Just <| ( step1, step3 ) :: generateSteps [ s2, { s3 | outsideShape = step3.statueAfterDissect.outsideShape } ]
 
-                            else
-                                ( step1, step3 ) :: generateSteps [ { s1 | outsideShape = step1.statueAfterDissect.outsideShape }, s2, { s3 | outsideShape = step3.statueAfterDissect.outsideShape } ]
-
-                        Nothing ->
-                            []
-
+                             else
+                                 Just <| ( step1, step3 ) :: generateSteps [ { s1 | outsideShape = step1.statueAfterDissect.outsideShape }, s2, { s3 | outsideShape = step3.statueAfterDissect.outsideShape } ]
+                        )
+                        |> Maybe.withDefault []
+                    )
         [ s1, s2 ] ->
-            case generateStep s1 s2 of
-                Just step ->
-                    [ step ]
-
-                Nothing ->
-                    []
+            generateStep s1 s2
+                |> Maybe.andThen (List.singleton >> Just)
+                |> Maybe.withDefault []
 
         _ ->
             []
