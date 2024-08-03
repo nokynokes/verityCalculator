9ef70127f3f50f67508cee66b538537b434d8b71
Nolan Cretney <nokynokes@gmail.com>
2024-08-02 19:22:52 -0600

    refactor + cleanup

--- src/View/Steps.elm
+++ src/View/Steps.elm
@@ -13,75 +13,72 @@ import Tailwind.Theme as Theme
 import Tailwind.Utilities as Tw
 
 
-stepOneBackGround : List Style
-stepOneBackGround =
-    [ Tw.bg_color Theme.zinc_700, Tw.border_solid, Tw.border_2, Tw.border_color Theme.slate_400, Tw.rounded, Tw.px_10 ]
+type StepType 
+    = DissectStep 
+    | AfterDissectStep
+
+statueName : StatueDissect -> Html Msg
+statueName dissect = 
+    let 
+        name = toString dissect.statueAfterDissect.position 
+    in
+        div [ css [ Tw.text_3xl, Tw.pb_3 ] ] [ text <| "On the " ++ String.toLower name ++ " statue:" ]
 
 stepForStatue : StatueDissect -> Html Msg
 stepForStatue dissect =
     let
-        statueName =
-            toString dissect.statueAfterDissect.position
-
         dissectShape =
             toString2D dissect.shapeToDissect
     in
     Html.div
         [ css [ Tw.text_color Theme.white ] ]
-        [ div [ css [ Tw.text_3xl, Tw.pb_3 ] ] [ text <| "On the " ++ String.toLower statueName ++ " statue:" ]
-        , div
-            [ css [ Tw.text_xl, Tw.py_3 ] ]
-            [ Html.p [] [ text <| "Dissect a " ++ String.toLower dissectShape ] ]
+        [ statueName dissect
+        , div [ css [ Tw.text_xl, Tw.py_3 ] ]
+              [ Html.p [] [ text <| "Dissect a " ++ String.toLower dissectShape ] ]
         ]
 
 afterDissect : StatueDissect -> Html Msg
 afterDissect dissect =
     let
-        statueName =
-            toString dissect.statueAfterDissect.position
-
         afterDissectShape =
             toString3D dissect.statueAfterDissect.outsideShape
     in
     Html.div
         [ css [ Tw.text_color Theme.white ] ]
-        [ div [ css [ Tw.text_3xl, Tw.pb_3 ] ] [ text <| "On the " ++ String.toLower statueName ++ " statue:" ]
-        , div
-            [ css [ Tw.text_xl, Tw.py_3 ] ]
-            [ Html.p [] [ text <| "There should be a " ++ afterDissectShape ]
-            , if isComplete dissect.statueAfterDissect.insideShape dissect.statueAfterDissect.outsideShape then
-                 Html.p []
-                         [ text <| statueName ++ " is done!"
-                         ]
-
-              else
-                Html.p [] [text ""]
-            ]
+        [ statueName dissect
+        , div [ css [ Tw.text_xl, Tw.py_3 ] ]
+              [ Html.p [] [ text <| "There should be a " ++ afterDissectShape ]
+              , if isComplete dissect.statueAfterDissect.insideShape dissect.statueAfterDissect.outsideShape then
+                  Html.p [] [ text "Statue is done!" ]
+                else
+                  Html.p [] [ text "" ]
+              ]
         ]
 
+renderStep : StepType -> Step -> Html Msg
+renderStep stepType ( statue1, statue2 ) =
+    div [ css [ Tw.flex, Tw.flex_row, Tw.gap_64, Bp.xxl [ Tw.justify_around ], Bp.xl [ Tw.justify_around ], Bp.lg [ Tw.justify_around ], Tw.justify_between ] ]
+        <| case stepType of
+             DissectStep -> [ stepForStatue statue1, stepForStatue statue2 ]
+             AfterDissectStep -> [ afterDissect statue1, afterDissect statue2 ]
 
+renderDissectStep : Step -> Html Msg
+renderDissectStep =
+    renderStep DissectStep
 
-renderShapesAfterStep : Step -> List (Html Msg)
-renderShapesAfterStep ( statue1, statue2 ) =
-    [afterDissect statue1, afterDissect statue2]
-
-
-renderStep : Step -> List (Html Msg)
-renderStep ( statue1, statue2 ) =
-    [stepForStatue statue1, stepForStatue statue2]
+renderShapesAfterDissect : Step -> Html Msg
+renderShapesAfterDissect =
+    renderStep AfterDissectStep
 
 renderSteps_ : Int -> Step -> Html Msg
 renderSteps_ stepNumber step = 
-    div
-        [ css stepOneBackGround ]
-        [ h1 [ css [ Tw.text_color Theme.white ] ] [ "Step " ++ String.fromInt (stepNumber + 1) |> text ]
-        , div [ css [ Tw.flex, Tw.flex_row, Tw.gap_64, Bp.xxl [ Tw.justify_around ], Bp.xl [ Tw.justify_around ], Bp.lg [ Tw.justify_around ], Tw.justify_between ] ] (renderStep step)
+    div [ css [ Tw.bg_color Theme.zinc_700, Tw.border_solid, Tw.border_2, Tw.border_color Theme.slate_400, Tw.rounded, Tw.px_10 ] ]
+        [ h1 [ css [ Tw.text_color Theme.white ] ] [ text <| "Step " ++ String.fromInt (stepNumber + 1) ]
+        , renderDissectStep step
         , hr [] []
-        , h2 [] [ text "After dissection: " ]
-        , div [ css [ Tw.flex, Tw.flex_row, Tw.gap_64, Bp.xxl [ Tw.justify_around ], Bp.xl [ Tw.justify_around ], Bp.lg [ Tw.justify_around ], Tw.justify_between ] ] (renderShapesAfterStep step)
+        , h2 [ css [ Tw.text_color Theme.white ] ] [ text "After dissection: " ]
+        , renderShapesAfterDissect step
         ]
-
-
 
 renderSteps : List Step -> List (Html Msg)
 renderSteps =
